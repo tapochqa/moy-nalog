fbc20cb32536c039d8fd211a3931204e4499af4c
Maxim <choochooh55@gmail.com>
2023-02-28 03:59:42 +0300

    clojars

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject moy-nalog "0.1.0"
+(defproject link.lmnd/moy-nalog "0.1.0"
   :description "lknpd.nalog.ru API wrapper"
   :url "https://lknpd.nalog.ru"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
